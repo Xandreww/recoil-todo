94270b4e1b84838b2313e958e6aa08eeec9f65a1
Andrzej <andrzej.popek@ec2.pl>
2021-05-22 15:16:53 +0200

    add task details view

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -1,19 +1,54 @@
-import { Container } from "@theme-ui/components";
 import React from "react";
+import { Container } from "@theme-ui/components";
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { getTodo } from "../recoil/selectors";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import "./TaskDetails.scss";
 
 const TaskDetails = ({ match }) => {
   const todo = useRecoilValue(getTodo(match.params.id));
 
+  const formatDate = (date) => {
+    const dateObj = new Date(date);
+    const month = dateObj.getUTCMonth() + 1; //months from 1-12
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+
+    const newdate = year + "/" + month + "/" + day;
+
+    return newdate;
+  };
+
   return (
-    <Container p={4} bg="muted">
-      <Link to="/">Go back</Link>
-      <h2>{todo.title}</h2>
-      <div>{todo.completed ? "completed" : "not yet completed"}</div>
-      <p>Created at: {todo.created_at}</p>
-      <p>Updated at: {todo.updated_at}</p>
+    <Container p={4} bg="muted" className="task-details">
+      <div className="task-details-inner">
+        <Link to="/" className="link-back">
+          <FontAwesomeIcon className="mr-10" icon={faArrowLeft} />
+          go back to task list
+        </Link>
+
+        <div className="task-details-header">
+          <div>
+            {todo.completed ? (
+              <>
+                <FontAwesomeIcon className="completed mr-10" icon={faCheckCircle} /> completed
+              </>
+            ) : (
+              <>
+                <FontAwesomeIcon className="not-completed mr-10" icon={faTimesCircle} /> not yet completed
+              </>
+            )}
+          </div>
+          <div className="create-dates">
+            <p>Created at: {formatDate(todo.created_at)}</p>
+            <p>Updated at: {formatDate(todo.updated_at)}</p>
+          </div>
+        </div>
+
+        <h2 className="task-details-text">{todo.title}</h2>
+      </div>
     </Container>
   );
 };
