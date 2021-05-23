7a4e5408b982e2d803c4fcfade830b7e35c671fa
Andrzej <andrzej.popek@ec2.pl>
2021-05-23 13:45:15 +0200

    add wildcard redirect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import { ThemeProvider } from "@theme-ui/theme-provider";
 import TodoList from "./components/TodoList/TodoList";
 import theme from "./shared/theme";
 import TaskDetails from "./components/TaskDetails/TaskDetails";
+import { useRecoilValue } from "recoil";
+import { getTodosState } from "./recoil/selectors";
 import "./App.scss";
 
 function App() {
+  const { totalNum } = useRecoilValue(getTodosState);
+
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <Route exact path="/" component={TodoList} />
-        <Route exact path="/task/:id" component={TaskDetails} />
-        <Route exact path="/task/:id/edit" component={TaskDetails} />
+        {totalNum && (
+          <>
+            <Route exact path="/task/:id" component={TaskDetails} />
+            <Route exact path="/task/:id/edit" component={TaskDetails} />
+          </>
+        )}
+        <Redirect to="/" />
       </ThemeProvider>
     </Router>
   );
